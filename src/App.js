7b0ebec5c987d0051c04e7463b0c764599e53417
sergioothoniel <sergioothoniel@hotmail.com>
2022-06-20 18:23:37 -0300

    create: home page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Apresentatio from "./components/apresentation";
+
+function App() {
+  return (
+    <div className="App">
+      <Apresentatio/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/apresentation/index.jsx
@@ -0,0 +1,12 @@
+import matrix from "../../assets/backgroundVideo.mp4"
+
+const Apresentatio = () =>{
+    return(
+        <div>
+            <video src={matrix} autoPlay loop></video>
+        </div>
+
+    )
+}
+
+export default Apresentatio
